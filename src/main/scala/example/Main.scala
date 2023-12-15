9d0acbd8f6a2ac8dd85ae063c831def00fa33199
Yanis <yanis.bmdn@gmail.com>
2023-12-15 10:44:57 +0100

    init ZIO base project

---
+++ src/main/scala/example/Main.scala
@@ -0,0 +1,13 @@
+import zio._
+import zio.stream._
+
+object App extends ZIOAppDefault {
+
+  override def run: ZIO[Any & (ZIOAppArgs & Scope), Any, Unit] =
+    for {
+      _ <- ZStream.fromIterable((1 to 10000).toList)
+        .take(10)
+        .map(_ * 2)
+        .foreach(Console.printLine(_))
+    } yield ()
+}
